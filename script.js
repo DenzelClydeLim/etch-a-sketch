9ff82acd3b35b95b0a19d8500c35309ee6b5d982
Denzel Clyde Lim <dcflim2020@plm.edu.ph>
2024-07-23 20:08:51 +0800

    Finished optional exercises from TOP
    Learned classList.contains. Specific reference to the target, not the id. Opacity. Prompt returns string. Template literals importance for declaring JS properties. Placement in complex loops.

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const container = document.querySelector("#container");
-const body = document.querySelector("body");
 const gridBtn = document.querySelector("#gridBtn");
-
-
+let num = 0;
 gridBtn.addEventListener("click", createGrid);
+let opacity = 0;
+container.addEventListener("mouseover", (e) => {
+    const tempGrid = e.target;
+    {
+        if (tempGrid.classList.contains("inDiv"))
+            tempGrid.style.backgroundColor = `rgb(${getColorNum()}, ${getColorNum()}, ${getColorNum()})`;
+        if (opacity <= 1) {
+            tempGrid.style.opacity = opacity;
+            opacity += 0.1
+        }
+    }
+});
+
+container.addEventListener("mouseout", (e) => {
+    const tempGrid = e.target;
+    if (tempGrid.classList.contains("inDiv")) {
+        tempGrid.style.backgroundColor = "transparent";
+    }
+});
 
 function deleteGrid() {
+    num = 0;
     container.textContent = "";
 }
 function getGridSize() {
@@ -16,20 +34,24 @@ function getGridSize() {
     return num;
 }
 
+function getColorNum() {
+    return Math.floor(Math.random() * 256);
+}
+
 function createGrid() {
     deleteGrid();
     const inputNum = getGridSize();
     const gridSize = 600 / inputNum;
-
-    for (i = 0; i < inputNum; i++) {
+    for (let i = 0; i < inputNum; i++) {
         const divFlex = document.createElement("div");
         divFlex.style.display = "flex";
-        divFlex.id = i;
-        for (j = 0; j < inputNum; j++) {
+        divFlex.id = `flexBox${i}`;
+        for (let j = 0; j < inputNum; j++) {
             const divBox = document.createElement("div");
             divBox.style.width = `${gridSize}px`;
             divBox.style.height = `${gridSize}px`;
-            divBox.classList.toggle("inDiv");
+            divBox.id = `divBox${num++}`;
+            divBox.classList.add("inDiv");
             divFlex.appendChild(divBox);
         }
         container.appendChild(divFlex);
